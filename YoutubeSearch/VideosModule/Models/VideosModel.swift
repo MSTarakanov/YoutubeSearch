60e96b68979224ac2e902b9cea62fd9e764090e0
Maxim Tarakanov <mstarakanov@yandex.ru>
2021-11-12 00:22:42 +0300

    added init for videosmodel

--- YoutubeSearch/VideosModule/Models/VideosModel.swift
+++ YoutubeSearch/VideosModule/Models/VideosModel.swift
@@ -8,5 +8,24 @@
 import Foundation
 
 struct Video {
+    let videoID: String
     let title: String
+    let channelTitle: String
+    let defaultThumbnailsUrl: String
+    
+    init?(item: Item) {
+        guard
+            let videoID = item.id?.videoID,
+            let title = item.snippet?.title,
+            let channelTitle = item.snippet?.channelTitle,
+            let defaultThumbnailsUrl = item.snippet?.thumbnails?.thumbnailsDefault?.url
+        else {
+            return nil
+        }
+        self.videoID = videoID
+        self.title = title
+        self.channelTitle = channelTitle
+        self.defaultThumbnailsUrl = defaultThumbnailsUrl
+    }
 }
+
